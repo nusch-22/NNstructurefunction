bb54dfe60f27e02860fe9c738f5e1c78c66666d7
nusch-22 <nusch22@gmx.de>
2022-04-24 17:34:53 +0200

    fixed Q2 cut

--- NNstructurefunctions.py
+++ NNstructurefunctions.py
@@ -28,30 +28,22 @@ fits_path.mkdir(exist_ok=True)
 
 def split_trval(x_data, y_data, perc=0.3):
     size_val = round(x_data.shape[0] * perc)
-    if size_val > 0:
-        idx = np.random.choice(
-            np.arange(1, x_data.shape[0] - 1, 2), size_val, replace=False
-        )
-        x_val = x_data[idx]
-        y_val = y_data[idx]
-        x_tr = np.delete(x_data, idx, axis=0)
-        y_tr = np.delete(y_data, idx)
-    else:
-        x_tr = x_data
-        y_tr = y_data
-        x_val = None
-        y_val = None
-    return x_tr, y_tr, x_val, y_val, size_val
+    idx = np.random.choice(
+        np.arange(1, x_data.shape[0] - 1, 2), size_val, replace=False
+    )
+    x_val = x_data[idx]
+    y_val = y_data[idx]
+    x_tr = np.delete(x_data, idx, axis=0)
+    y_tr = np.delete(y_data, idx)
+    return x_tr, y_tr, x_val, y_val
 
 
-def load_data(
-    Q2_cut=None,
-):  # Q2 cut does not work yet bc of empty x_val in concetanation
+def load_data(Q2_cut=None):
     filenames = os.listdir(f"{current_path}/data")
     # filenames = ["DATA_CHORUS_0.02.yaml"]
 
-    x_all_data = []
-    y_all_data = []
+    x_sep_data = []
+    y_sep_data = []
 
     for i, filename in enumerate(filenames):
         with open(f"{current_path}/data/" + filename, "r") as file:
@@ -62,45 +54,64 @@ def load_data(
         F_2 = np.array(input_data["F_2"])
         F_2_err_stat = np.array(input_data["F_2_err_stat"])
         F_2_err_sys = np.array(input_data["F_2_err_sys"])
-        x_data_new = np.zeros((len(Q2), 2))
 
         if Q2_cut != None:
             Q2_mask = np.where(Q2 < Q2_cut)
             Q2 = Q2[Q2_mask]
             F_2 = F_2[Q2_mask]
-            F_2_err = F_2_err[Q2_mask]
+            F_2_err_stat = F_2_err_stat[Q2_mask]
+            F_2_err_sys = F_2_err_sys[Q2_mask]
 
         if i == 0:
             x_data = np.zeros((len(Q2), 2))
             x_data[:, 0] = x
             x_data[:, 1] = Q2
             y_data = F_2
-            x_all_data.append(x_data)
-            y_all_data.append(y_data)
+            x_sep_data.append(x_data)
+            y_sep_data.append(y_data)
             y_err_stat = F_2_err_stat
             y_err_sys = F_2_err_sys
-            x_tr, y_tr, x_val, y_val, size_val = split_trval(x_data, y_data)
+
+            if x_data.shape[0] >= 3:
+                x_tr, y_tr, x_val, y_val = split_trval(x_data, y_data)
+                val = True
+            else:
+                x_tr = x_data
+                y_tr = y_data
+                val = False
         else:
+            x_data_new = np.zeros((len(Q2), 2))
             x_data_new[:, 0] = x
             x_data_new[:, 1] = Q2
             y_data_new = F_2
-            x_all_data.append(x_data)
-            y_all_data.append(y_data)
             y_err_stat_new = F_2_err_stat
             y_err_sys_new = F_2_err_sys
-            x_tr_new, y_tr_new, x_val_new, y_val_new, size_val = split_trval(
-                x_data_new, y_data_new
-            )
+
+            if x_data_new.shape[0] >= 3:
+                x_tr_new, y_tr_new, x_val_new, y_val_new = split_trval(
+                    x_data_new, y_data_new
+                )
+                if val:
+                    x_val = np.concatenate([x_val, x_val_new], axis=0)
+                    y_val = np.concatenate([y_val, y_val_new], axis=0)
+                else:
+                    x_val = x_val_new
+                    y_val = y_val_new
+                    val = True
+
+            else:
+                x_tr_new = x_data_new
+                y_tr_new = y_data_new
 
             x_tr = np.concatenate([x_tr, x_tr_new], axis=0)
             y_tr = np.concatenate([y_tr, y_tr_new], axis=0)
             y_err_stat = np.concatenate([y_err_stat, y_err_stat_new], axis=0)
             y_err_sys = np.concatenate([y_err_sys, y_err_sys_new], axis=0)
+
+            x_sep_data.append(x_data_new)
+            y_sep_data.append(y_data_new)
             x_data = np.concatenate([x_data, x_data_new], axis=0)
             y_data = np.concatenate([y_data, y_data_new], axis=0)
-            if size_val > 0:
-                x_val = np.concatenate([x_val, x_val_new], axis=0)
-                y_val = np.concatenate([y_val, y_val_new], axis=0)
 
     return {
         "x_tr": x_tr,
@@ -111,8 +122,8 @@ def load_data(
         "y_data": y_data,
         "y_err_stat": y_err_stat,
         "y_err_sys": y_err_sys,
-        "x_sep_data": x_all_data,
-        "y_sep_data": y_all_data,
+        "x_sep_data": x_sep_data,
+        "y_sep_data": y_sep_data,
     }
 
 
@@ -138,8 +149,8 @@ def chi2_with_covmat(covmat, ndata):
     def custom_loss(y_true, y_pred):
         # (yt - yp) * covmat * (yt - yp)
         tmp = y_true - y_pred
-        right_dot = K.tf.tensordot(invcovmat, K.transpose(tmp), axes=1)
-        return K.tf.tensordot(tmp, right_dot, axes=1) / ndata
+        right_dot = tf.tensordot(invcovmat, K.transpose(tmp), axes=1)
+        return tf.tensordot(tmp, right_dot, axes=1) / ndata
 
     return custom_loss
 
@@ -161,9 +172,7 @@ def model_trainer(data_dict, **hyperparameters):
     # Compile the Model as usual
     ndata = data_dict["y_data"].shape[0]
     covmat = compute_covmat(data_dict)
-    model.compile(
-        loss=chi2_with_covmat(covmat, ndata), optimizer=optimizer, metrics=["accuracy"]
-    )
+    model.compile(loss="mse", optimizer=optimizer, metrics=["accuracy"])
 
     # Callbacks for Early Stopping
     ES = EarlyStopping(
@@ -190,7 +199,7 @@ def model_trainer(data_dict, **hyperparameters):
     # This might be ideal if one have very small number of datapoints
     scores = model.evaluate(data_dict["x_val"], data_dict["y_val"], verbose=0)
     # Return the value of the validation loss
-    return model, scores[0], data_dict
+    return model, scores[0]
 
 
 def define_hyperspace():
@@ -228,7 +237,7 @@ def perform_hyperopt(data_dict, nb_trials=2):
 
     # Define the hyperoptimization function
     def hyper_function(hyperspace_dict):
-        _, val_loss, _ = model_trainer(data_dict, **hyperspace_dict)
+        _, val_loss = model_trainer(data_dict, **hyperspace_dict)
         return {"loss": val_loss, "status": "ok"}
 
     trials = FileTrials(hyperopt_path, parameters=hyperspace)
@@ -285,25 +294,27 @@ def plot_constant_x_reps(x_data, y_data, y_err, x_grid, p_mid, p_err):
 
 def plot_constant_x(best_model, data_dict):
     # loop over x values
-    for i, x in enumerate(data_dict["x_sep_data"]):
+    for i in range(len(data_dict["x_sep_data"])):
+        x = data_dict["x_sep_data"][i]
         y = data_dict["y_sep_data"][i]
         x_grid = np.linspace(x[0], x[-1], 100)
         y_pred = best_model(x_grid)
 
-        plt.figure()
-        plt.plot(x_grid[:, 1], y_pred, color="red", label="Prediction")
-        plt.scatter(x[:, 1], y, color="blue", label="Data")
-        plt.legend()
-        plt.xlabel("$Q^2$ [GeV$^2$]")
-        plt.ylabel("$F_2$")
-        plt.title(f"Prediction of $F_2$ at $x={x[0,0]}$")
+        fig, ax = plt.subplots(1, 1)
+        ax.plot(x_grid[:, 1], y_pred, color="red", label="Prediction")
+        ax.scatter(x[:, 1], y, color="blue", label="Data")
+        ax.legend()
+        ax.set_xlabel("$Q^2$ [GeV$^2$]")
+        ax.set_ylabel("$F_2$")
+        ax.set_title(f"Prediction of $F_2$ at $x={x[0,0]}$")
         plt.savefig(f"{fits_path}/FIT_{x[0,0]}.png")
+        ax.clear()
 
 
-data_dict = load_data()
+data_dict = load_data(Q2_cut=30)
 best_params = perform_hyperopt(data_dict)
 
 # create_replicas()
 
-# best_model, _, data_dict = model_trainer(data_dict, **best_params)
-# plot_constant_x(best_model, data_dict)
+best_model, _ = model_trainer(data_dict, **best_params)
+plot_constant_x(best_model, data_dict)
